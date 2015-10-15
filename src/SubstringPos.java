87e5cc9d160fb63824d83e732e743e3f471cb38f
Pavel Raschetnov <pasharvs@gmail.com>
2015-10-15 19:22:05 +0300

    Made Searcher class and methods static + fixed bug
    bug in getPos() method:   case when stubString matches "0+"

--- src/SubstringPos.java
+++ src/SubstringPos.java
@@ -8,100 +8,93 @@ public class SubstringPos {
         Scanner scanner = new Scanner(System.in);
 
         String s = scanner.nextLine();
-        Searcher searcher = new Searcher(s);
-        System.out.println(searcher.getPos());
-
-
+        System.out.println(Searcher.getPos(s));
     }
 
-}
+    static class Searcher {
 
-class Searcher {
-    private String subString;
+        public static int getPos(String subString) {
+            if (subString.matches("0+")) {
+                int n = Integer.parseInt("1" + subString);
+                return index(n) + 1;
+            }
 
-    public Searcher(String subString) {
-        this.subString = subString;
-    }
-
-    public int getPos() {
-        if (subString.matches("0+")) {
-            this.subString = this.subString + "1";
-            return getPos();
-        }
-        int bestPos;
-        for (int len = 1; len <= subString.length(); len++) {
-            bestPos = Integer.MAX_VALUE;
-            for (int start = 0; start < len; start++) {
-                int firstNumber = getFirstNumber(start, len);
-                if (firstNumber == -1) {
-                    continue;
+            int bestPos;
+            for (int len = 1; len <= subString.length(); len++) {
+                bestPos = Integer.MAX_VALUE;
+                for (int start = 0; start < len; start++) {
+                    int firstNumber = getFirstNumber(subString, start, len);
+                    if (firstNumber == -1) {
+                        continue;
+                    }
+                    String generatedString = generateString(subString, start, firstNumber);
+                    if (subString.equals(generatedString)) {
+                        //position found for current len, start
+                        int pos =  index(firstNumber) - start;
+                        if (pos < bestPos) bestPos = pos;
+                    }
                 }
-                String generatedString = generateString(start, firstNumber);
-                if (this.subString.equals(generatedString)) {
-                    //position found
-                    int pos =  index(firstNumber) - start;
-                    if (pos < bestPos) bestPos = pos;
+
+                if (bestPos < Integer.MAX_VALUE)  {
+                    return bestPos;
                 }
             }
-            if (bestPos < Integer.MAX_VALUE)  {
-                return bestPos;
-            }
-        }
-        return -1;
-    }
-
-    private int getFirstNumber(int start, int len) {
-        String firstNumberStr;
-        if (start + len <= subString.length()) {
-            //there is a full number in subString
-            firstNumberStr = subString.substring(start, start + len);
-        } else {
-            //create full number combining end of previous number and beginning of next number
-            String prevNumberEnd = subString.substring(0, start);
-            String prevNumberEndPlus = String.valueOf(Integer.parseInt(prevNumberEnd) + 1);
-            while (prevNumberEndPlus.length() < prevNumberEnd.length()) {
-                prevNumberEndPlus = "0" + prevNumberEndPlus;
-            }
-
-            String a = subString.substring(start, subString.length());
-            String b = prevNumberEndPlus.substring(prevNumberEndPlus.length() - (len - a.length()));
-            firstNumberStr = a + b;
-        }
-        if (firstNumberStr.charAt(0) == '0') {
             return -1;
         }
 
-        return Integer.parseInt(firstNumberStr);
-    }
+        private static int getFirstNumber(String subString, int start, int len) {
+            String firstNumberStr;
+            if (start + len <= subString.length()) {
+                //there is a full number in subString
+                firstNumberStr = subString.substring(start, start + len);
+            } else {
+                //create full number combining end of previous number and beginning of next number
+                String prevNumberEnd = subString.substring(0, start);
+                String prevNumberEndPlus = String.valueOf(Integer.parseInt(prevNumberEnd) + 1);
+                while (prevNumberEndPlus.length() < prevNumberEnd.length()) {
+                    prevNumberEndPlus = "0" + prevNumberEndPlus;
+                }
 
-    private String generateString(int start, int firstNumber) {
-        StringBuilder sb = new StringBuilder("" + firstNumber);
-        String prevNumber = String.valueOf(firstNumber - 1);
-        String prevNumberEnding = prevNumber.substring(prevNumber.length() - start);
-        sb.insert(0, prevNumberEnding);
+                String a = subString.substring(start, subString.length());
+                String b = prevNumberEndPlus.substring(prevNumberEndPlus.length() - (len - a.length()));
+                firstNumberStr = a + b;
+            }
+            if (firstNumberStr.charAt(0) == '0') {
+                return -1;
+            }
 
-        while (sb.length() < this.subString.length()) {
-            firstNumber++;
-            sb.append(String.valueOf(firstNumber));
+            return Integer.parseInt(firstNumberStr);
         }
-        sb.setLength(this.subString.length());
 
-        return sb.toString();
-    }
+        private static String generateString(String subString, int start, int firstNumber) {
+            StringBuilder sb = new StringBuilder("" + firstNumber);
+            String prevNumber = String.valueOf(firstNumber - 1);
+            String prevNumberEnding = prevNumber.substring(prevNumber.length() - start);
+            sb.insert(0, prevNumberEnding);
 
-    private int index(int n) {
-        int x = 1;
-        int m = 0;
-        int xIndex = 1;
-        while (n > x) {
-            m++;
-            xIndex += 9 * x * m;
-            x *= 10;
+            while (sb.length() < subString.length()) {
+                firstNumber++;
+                sb.append(firstNumber);
+            }
+            sb.setLength(subString.length());
+
+            return sb.toString();
         }
-        return xIndex - (x - n) * m;
+
+        private static int index(int n) {
+            int x = 1;
+            int m = 0;
+            int xIndex = 1;
+            while (n > x) {
+                m++;
+                xIndex += 9 * x * m;
+                x *= 10;
+            }
+            return xIndex - (x - n) * m;
+        }
     }
-
-
 
 
 }
+
+
